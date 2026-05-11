beb771da08ba0173ed99fedd7bed39e32cc238e6
Clay <jjota7778@gmail.com>
2026-05-10 23:48:27 -0500

    Adjust online user

--- modules/right_panel.lua
+++ modules/right_panel.lua
@@ -61,7 +61,7 @@ function RightPanel.Create(parent, Theme)
         local avatarHolder = Instance.new("Frame")
         avatarHolder.Name = "AvatarHolder"
         avatarHolder.Size = UDim2.new(0, 34, 0, 34)
-        avatarHolder.Position = UDim2.new(0, 2, 0.5, -17)
+        avatarHolder.Position = UDim2.new(0, -2, 0.5, -17)
         avatarHolder.BackgroundColor3 = Theme.Colors.Panel
         avatarHolder.BorderSizePixel = 0
         avatarHolder.Parent = row
