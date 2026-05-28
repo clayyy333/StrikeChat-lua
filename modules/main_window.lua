e524616ba81f6bdd14a93a50b6d5e461ce67c560
Clay <jjota7778@gmail.com>
2026-05-28 12:42:53 -0500

    qwe

--- modules/main_window.lua
+++ modules/main_window.lua
@@ -24,7 +24,7 @@ function MainWindow.Create(CoreGui, Theme)
     backgroundImage.Size = UDim2.new(1, 0, 1, 0)
     backgroundImage.Position = UDim2.new(0, 0, 0, 0)
     backgroundImage.BackgroundTransparency = 1
-    backgroundImage.Image = "rbxassetid://75491780980123"
+    backgroundImage.Image = "rbxassetid://112209908757733"
     backgroundImage.ScaleType = Enum.ScaleType.Crop
     backgroundImage.ImageTransparency = 0
     backgroundImage.ZIndex = 1
